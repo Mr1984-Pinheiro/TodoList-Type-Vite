ea90749cabefe10746a9efe9f579b70de8fcbb06
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-02-28 15:48:27 -0300

    função delete e completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
@@ -10,18 +10,45 @@ export type Todo = {
 }
 
 function App() {
+
+  //conectar o input ao valor do estado - pegar valor da variavel
+  const [todoInput, setTodoInput] = useState('');  
   const [todos, setTodos] = useState<Todo[]>([]); //<> é generic
+
+  function addTodo () {
+    //pegar variavel de input e jogar na lista de todos usando previous
+    setTodos((previousTodos) =>
+     [...previousTodos, { id: Math.random(), title: todoInput, completed: false }]
+     );
+    setTodoInput('');
+  }
+
+  function completeTodo (id: number) {
+      setTodos((previousTodos) => 
+      previousTodos.map(( todo ) => todo.id !== id ? todo : { ...todo, completed: !todo.completed}));
+      setTodoInput('');
+  }
+
+  function handleInputChange (e: ChangeEvent<HTMLInputElement> ) {
+    //Para alterar o valor estatico do input 
+    setTodoInput(e.target.value);
+  }
+
+  function deleteTodo (id: number) {
+    setTodos((previousTodos) => previousTodos.filter((todo) => todo.id !== id ));
+  }
 
   return (
     <div className="App">
       <div className='add-todo'>
-        <input placeholder='Fazer café' />
-        <button>Adicionar</button>
+        {/*conecar input ao estado precisa 2 atributos value e onChange */}
+        <input placeholder='Fazer café' value={todoInput} onChange={handleInputChange} />
+        <button onClick={addTodo}>Adicionar</button>
       </div>
 
       {
          todos.map((todo) => (
-           <Card key={todo.id} todo={todo} />
+           <Card key={todo.id} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
          ))
           
       }
